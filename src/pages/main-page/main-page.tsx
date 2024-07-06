67f0f565089ac0cde4ca933cfc8fec95bf08a987
Anna Kapitanova <totoshechka19@gmail.com>
2024-07-06 11:11:54 +0300

    еще исправила на tabIndex={0}

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -88,7 +88,7 @@ function MainPage({offersCount}: MainPageProps): JSX.Element {
                   </svg>
                 </span>
                 <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex="0">Popular</li>
+                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
                   <li className="places__option" tabIndex={0}>Price: low to high</li>
                   <li className="places__option" tabIndex={0}>Price: high to low</li>
                   <li className="places__option" tabIndex={0}>Top rated first</li>

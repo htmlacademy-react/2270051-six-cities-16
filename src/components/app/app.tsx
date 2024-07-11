9fc337761896f9845d6f480549b9e7ccea182dec
Anna Kapitanova <totoshechka19@gmail.com>
2024-07-11 16:08:18 +0300

    добавила все маршруты

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,26 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
 
 type AppPageProps = {
   offersCount: number;
   cardsCount: number;
 }
 
-function App({offersCount, cardsCount }: AppPageProps) {
+function App({ offersCount, cardsCount }: AppPageProps) {
   return (
-    <MainPage offersCount={offersCount} cardsCount={cardsCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage offersCount={offersCount} cardsCount={cardsCount} />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/offer/:id" element={<OfferPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

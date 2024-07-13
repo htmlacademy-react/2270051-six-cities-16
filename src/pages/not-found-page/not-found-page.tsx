644cb5d116caf9a8a4314e7a60a28cce564d2cc5
Anna Kapitanova <totoshechka19@gmail.com>
2024-07-13 15:10:59 +0300

    доработала страницу 404, добавила header как компонент на страницах favorites и offer

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,11 +1,23 @@
 import { Link } from 'react-router-dom';
+import Header from '../../components/header/header';
 
 function NotFoundPage() {
   return (
-    <main className="page__main">
-      <h1>404. Page not found</h1>
-      <Link to="/">Вернуться на главную</Link>
-    </main>
+    <div className="page page--favorites-empty">
+      <Header />
+
+      <main className="page__main page__main--favorites page__main--favorites-empty">
+        <div className="page__favorites-container container">
+          <section className="favorites favorites--empty">
+            <h1 className="visually-hidden">Page not found (empty)</h1>
+            <div className="cities__status-wrapper">
+              <b className="favorites__status">404. Page not found</b>
+              <Link to="/">Go back to main page</Link>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
   );
 }
 

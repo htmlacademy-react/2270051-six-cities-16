281423f7871b7bed7d710085158c3d25c4898233
Anna Kapitanova <totoshechka19@gmail.com>
2024-07-15 15:43:35 +0300

    Подключила моки offers в index.tsx. Передала их в виде props компоненту (App).

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import {Setting} from './const';
+import {OFFERS} from './mocks/offers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +12,7 @@ root.render(
   <React.StrictMode>
     <App
       offersCount = {Setting.OffersCount}
+      offers={OFFERS}
     />
   </React.StrictMode>
 );

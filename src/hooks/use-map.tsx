bbed905b80d9ce06cd26382699b84ac0dc5ea7c7
Anna Kapitanova <totoshechka19@gmail.com>
2024-07-19 16:49:32 +0300

    немного поправила useMap

--- src/hooks/use-map.tsx
+++ src/hooks/use-map.tsx
@@ -1,26 +1,23 @@
 import {useEffect, useState, MutableRefObject, useRef} from 'react';
-import leaflet from 'leaflet';
+import {Map, TileLayer} from 'leaflet';
 import {City} from '../lib/types/offer';
 
 function useMap(
   mapRef: MutableRefObject<HTMLElement | null>,
   city: City
-): leaflet.Map | null {
-  const [map, setMap] = useState<leaflet.Map | null>(null);
+): Map | null {
+  const [map, setMap] = useState<Map | null>(null);
   const isRenderedRef = useRef<boolean>(false);
 
   useEffect(() => {
     if (mapRef.current !== null && !isRenderedRef.current) {
-      const instance = new leaflet.Map(mapRef.current, {
+      const instance = new Map(mapRef.current , {
         center: [city.location.latitude, city.location.longitude],
         zoom: city.location.zoom
       });
 
-      const layer = new leaflet.TileLayer(
+      const layer = new TileLayer(
         'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
-        {
-          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        },
       );
 
       instance.addLayer(layer);

465c977527a7b3250773f3455d86386026699ba6
Anna Kapitanova <totoshechka19@gmail.com>
2024-08-11 11:59:57 +0300

    рефакторинг type State

--- src/lib/types/state.ts
+++ src/lib/types/state.ts
@@ -1,11 +1,11 @@
 import {BaseOffer, City} from './offer';
-import {AuthorizationStatus} from '../../const';
+import {AuthorizationStatus, RequestStatus} from '../../const';
 import {AuthorizationUser} from './user';
 
 export type State = {
   city: City;
   offers: BaseOffer[];
-  status: 'idle' | 'loading' | 'succeeded' | 'failed';
+  status: RequestStatus;
   error: string | null;
   authorizationStatus: keyof typeof AuthorizationStatus;
   authorizationUser: AuthorizationUser | null;

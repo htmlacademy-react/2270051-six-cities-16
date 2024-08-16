a013f8300707d61d90d331558b50ea9d00b902f4
Anna Kapitanova <totoshechka19@gmail.com>
2024-08-16 18:15:24 +0300

    исправила ошибки

--- src/hooks/use-user.ts
+++ src/hooks/use-user.ts
@@ -1,11 +1,11 @@
 import {useAppSelector} from './redux-hooks';
-import {User} from '../lib/types/review';
 import {RootState} from '../store';
 import {AuthorizationStatus} from '../const';
+import {AuthorizationUser} from '../lib/types/user';
 
 export function useUser(): {
   authorizationStatus: typeof AuthorizationStatus[keyof typeof AuthorizationStatus];
-  authorizationUser?: User;
+  authorizationUser?: AuthorizationUser;
   } {
   const authorizationStatus = useAppSelector((state:RootState) => state.user.authorizationStatus);
   const authorizationUser = useAppSelector((state: RootState) => state.user.authorizationUser);

ef77c5370d8b0efc49786c40cd1cfb2a239f448b
Anna Kapitanova <totoshechka19@gmail.com>
2024-08-19 18:53:27 +0300

    исправление ошибки

--- src/components/comment-form/rating.tsx
+++ src/components/comment-form/rating.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 type RatingProps = {
   rating: string;
   handleRatingChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  disabled?: boolean;
 }
 
-function Rating({rating, handleRatingChange}: RatingProps) {
+function Rating({ rating, handleRatingChange, disabled }: RatingProps) {
   return (
     <div className="reviews__rating-form form__rating">
       {[5, 4, 3, 2, 1].map((value) => (
@@ -18,13 +19,12 @@ function Rating({rating, handleRatingChange}: RatingProps) {
             type="radio"
             onChange={handleRatingChange}
             checked={rating === value.toString()}
-            key={`input-${value}`}
+            disabled={disabled}
           />
           <label
             htmlFor={`${value}-stars`}
             className="reviews__rating-label form__rating-label"
             title={['perfect', 'good', 'not bad', 'badly', 'terribly'][5 - value]}
-            key={`label-${value}`}
           >
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
